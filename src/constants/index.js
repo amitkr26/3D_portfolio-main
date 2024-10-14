ee5e5531f297c4b56b8531fca09e551ad396ac73
Amit Kumar <amitkrbsc26@gmail.com>
2024-10-14 08:53:08 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,12 +173,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/AzadG',
+        link: 'https://github.com/AzadG26',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/AzadG',
+        link: 'https://www.linkedin.com/in/AzadG26',
     }
 ];
 
@@ -188,41 +188,41 @@ export const projects = [
         theme: 'btn-back-red',
         name: 'Amazon Price Tracker',
         description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/AzadG/pricewise',
+        link: 'https://github.com/AzadG26/pricewise',
     },
     {
         iconUrl: threads,
         theme: 'btn-back-green',
         name: 'Full Stack Threads Clone',
         description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/AzadG/threads',
+        link: 'https://github.com/AzadG26/threads',
     },
     {
         iconUrl: car,
         theme: 'btn-back-blue',
         name: 'Car Finding App',
         description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/AzadG/project_next13_car_showcase',
+        link: 'https://github.com/AzadG26/project_next13_car_showcase',
     },
     {
         iconUrl: snapgram,
         theme: 'btn-back-pink',
         name: 'Full Stack Instagram Clone',
         description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/AzadG/social_media_app',
+        link: 'https://github.com/AzadG26/social_media_app',
     },
     {
         iconUrl: estate,
         theme: 'btn-back-black',
         name: 'Real-Estate Application',
         description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/AzadG/projects_realestate',
+        link: 'https://github.com/AzadG26/projects_realestate',
     },
     {
         iconUrl: summiz,
         theme: 'btn-back-yellow',
         name: 'AI Summarizer Application',
         description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/AzadG/project_ai_summarizer',
+        link: 'https://github.com/AzadG26/project_ai_summarizer',
     }
 ];
